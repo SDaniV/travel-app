35336812e952c2824b155b44e9aa1bd76a8a037f
SDaniV <sorokadanylo1@gmail.com>
2021-12-24 16:01:18 +0200

    Fix indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	get '/tags' => 'tags#index'
-	get '/tags/:id' => 'tags#show', as: :tag
+  get '/tags' => 'tags#index'
+  get '/tags/:id' => 'tags#show', as: :tag
   get '/destinations/:id' => 'destinations#show', as: :destination
   get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination
   patch '/destinations/:id' => 'destinations#update'
